e3116e01142e151cdcd49d04a8b28f2a8b950dc0
nb-0310 <nirzarbhatt031093@gmail.com>
2024-07-12 09:43:44 +0530

    Plugin Commit

---
+++ word-filter.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Word Filter
+Description: This is my second plugin
+Version: 1.0
+Author Nirzar
+Author URI: https://nirzarbhatt.netlify.app/
+Text Domain: wcpdomain
+Domain Path: /languages
+*/
